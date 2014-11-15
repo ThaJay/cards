53697eb712e575621c2e3ddb98e27e462e4ef2b3
ThaJay <thajaygit@gmail.com>
2014-11-15 17:51:02 +0100

    geen moer
    een basis ofzo

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
 ﻿window.onload = function ()
 {
+    var kaarten =
+        {
+            card2C: "cards/Clubs/2C.svg",
+            card3C: "cards/Clubs/3C.svg",
+            card4C: "cards/Clubs/4C.svg",
+            card5C: "cards/Clubs/5C.svg",
+            card6C: "cards/Clubs/6C.svg",
+            card7C: "cards/Clubs/7C.svg",
+            card8C: "cards/Clubs/8C.svg",
+            card9C: "cards/Clubs/9C.svg",
+            card10C: "cards/Clubs/10C.svg",
+            cardJC: "cards/Clubs/JC.svg",
+            cardQC: "cards/Clubs/QC.svg",
+            cardKC: "cards/Clubs/KC.svg",
+            cardAC: "cards/Clubs/AC.svg",
 
+            card2D: "cards/Diamonds/2D.svg",
+            card3D: "cards/Diamonds/3D.svg",
+            card4D: "cards/Diamonds/4D.svg",
+            card5D: "cards/Diamonds/5D.svg",
+            card6D: "cards/Diamonds/6D.svg",
+            card7D: "cards/Diamonds/7D.svg",
+            card8D: "cards/Diamonds/8D.svg",
+            card9D: "cards/Diamonds/9D.svg",
+            card10D: "cards/Diamonds/10D.svg",
+            cardJD: "cards/Diamonds/JD.svg",
+            cardQD: "cards/Diamonds/QD.svg",
+            cardKD: "cards/Diamonds/KD.svg",
+            cardAD: "cards/Diamonds/AD.svg",
 
+            card2D: "cards/Hearts/2D.svg",
+            card3D: "cards/Hearts/3D.svg",
+            card4D: "cards/Hearts/4D.svg",
+            card5D: "cards/Hearts/5D.svg",
+            card6D: "cards/Hearts/6D.svg",
+            card7D: "cards/Hearts/7D.svg",
+            card8D: "cards/Hearts/8D.svg",
+            card9D: "cards/Hearts/9D.svg",
+            card10D: "cards/Hearts/10D.svg",
+            cardJD: "cards/Hearts/JD.svg",
+            cardQD: "cards/Hearts/QD.svg",
+            cardKD: "cards/Hearts/KD.svg",
+            cardAD: "cards/Hearts/AD.svg",
+
+            card2S: "cards/Spades/2S.svg",
+            card3S: "cards/Spades/3S.svg",
+            card4S: "cards/Spades/4S.svg",
+            card5S: "cards/Spades/5S.svg",
+            card6S: "cards/Spades/6S.svg",
+            card7S: "cards/Spades/7S.svg",
+            card8S: "cards/Spades/8S.svg",
+            card9S: "cards/Spades/9S.svg",
+            card10S: "cards/Spades/10S.svg",
+            cardJS: "cards/Spades/JS.svg",
+            cardQS: "cards/Spades/QS.svg",
+            cardKS: "cards/Spades/KS.svg",
+            cardAS: "cards/Spades/AS.svg"
+        },
+        kaart = "0"
+    document.getElementByClassName("kaart").src.replace("cards/Clubs/AC.svg", "cards/Spades/AS.svg");
+
+    
 
 };
